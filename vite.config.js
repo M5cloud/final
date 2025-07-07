f4343e1ffd7095876ab7ec3f8744f470420a4bfe
m5cloud <muhammadyousaf0263706@gmail.com>
2025-07-07 10:23:16 +0500

    Deply setting changed

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/hello/', // <-- your repo name here
+    base: '/final/', // <-- your repo name here
 
   plugins: [react()],
 })
