08a491d4cb3ba8f5b725897b0b2632bd4374fce8
M5cloud <muhammadyousaf0263706@gmail.com>
2025-07-07 11:40:10 +0500

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: '/final/', // 👈 this MUST match your GitHub repo name exactly
-  plugins: [react()],
-})
